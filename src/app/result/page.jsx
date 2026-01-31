39e56c5d59e9f6a4e53c146301994932bedbd47b
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2026-01-31 23:45:06 +0600

    try catch on api calls

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,7 +1,12 @@
 import { getProducts } from "@/lib/services/products";
 import ResultContent from "./ResultContent";
+import { Suspense } from "react";
 
 export default async function Result() {
   const products = await getProducts({ limit: 20, offset: 0 });
-  return <ResultContent products={products} />;
+  return (
+    <Suspense>
+      <ResultContent products={products} />
+    </Suspense>
+  );
 }
