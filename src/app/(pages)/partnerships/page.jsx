fc8d82380a9059e65876e5669dde487b442af823
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2026-02-01 20:16:44 +0600

    update animation on partnerships and support us page

--- src/app/(pages)/partnerships/page.jsx
+++ src/app/(pages)/partnerships/page.jsx
@@ -4,16 +4,30 @@ import { OurCoreEngine } from "@/app/components/parnetships/OurCoreEngine";
 import { PartnerInWorldBuilding } from "@/app/components/parnetships/PartnerInWorldBuilding";
 import PartnershipsHeader from "@/app/components/parnetships/PartnershipsHeader";
 import { ProofOfConcept } from "@/app/components/parnetships/ProofOfConcept";
+import { AnimatedWrapper } from "../../components/shared/AnimatedWrapper";
+import { fadeUp } from "../../utils/animations";
 
 const PartnershipsPage = () => {
   return (
     <>
-      <PartnershipsHeader />
-      <OurCoreEngine />
-      <ProofOfConcept />
-      <InvestorsAndPartners />
-      <PartnerInWorldBuilding />
-      <ContractOurBusinessTeam />
+      <AnimatedWrapper variant={fadeUp} delay={0}>
+        <PartnershipsHeader />
+      </AnimatedWrapper>
+      <AnimatedWrapper variant={fadeUp} delay={0.1}>
+        <OurCoreEngine />
+      </AnimatedWrapper>
+      <AnimatedWrapper variant={fadeUp} delay={0.2}>
+        <ProofOfConcept />
+      </AnimatedWrapper>
+      <AnimatedWrapper variant={fadeUp} delay={0.3}>
+        <InvestorsAndPartners />
+      </AnimatedWrapper>
+      <AnimatedWrapper variant={fadeUp} delay={0.4}>
+        <PartnerInWorldBuilding />
+      </AnimatedWrapper>
+      <AnimatedWrapper variant={fadeUp} delay={0.5}>
+        <ContractOurBusinessTeam />
+      </AnimatedWrapper>
     </>
   );
 };
