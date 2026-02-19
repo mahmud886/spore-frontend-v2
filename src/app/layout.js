3bb01c2ccdfbeedc3ed3e3a48d33f5caf59fe1ce
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2026-02-20 00:32:58 +0600

    update favicon icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -63,6 +63,29 @@ export const metadata = {
       "A deadly pathogen threatens to overrun the nation city of Lionara. Join the resistance or embrace the evolution.",
     images: ["/api/og"],
   },
+  icons: {
+    icon: [
+      { url: "/assets/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/assets/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    shortcut: "/assets/favicon_io/favicon.ico",
+    apple: [{ url: "/assets/favicon_io/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/assets/favicon_io/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/assets/favicon_io/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/assets/favicon_io/site.webmanifest",
   robots: {
     index: true,
     follow: true,
